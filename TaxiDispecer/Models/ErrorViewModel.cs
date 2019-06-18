3dc26646be9c583cfc35c79ab8c241cc6e83a57a
Arslan Turkusic <44257080+aturkusic@users.noreply.github.com>
2019-06-18 15:15:32 +0200

    Add files via upload

---
+++ TaxiDispecer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TaxiDispecer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ TaxiDispecer/Models/Osoba.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaxiDispecer.Models
+{
+    public enum Spol
+    {
+        Muski,
+        Zenski
+    }
+
+    public class Osoba
+    {
+        public int ID { get; set; }
+    
+        public string Ime { get; set; }
+       
+        public string Prezime { get; set; }
+       
+        public DateTime? DatumRodjenja { get; set; }
+      
+        public string JMBG { get; set; }
+    
+        public Spol Spol { get; set; }
+     
+        public string EMail { get; set; }
+
+    }
+}
