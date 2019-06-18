9d581ba3e1d9c56b9a1005a86de1c427ad030c48
aturkusic <aturkusic1@etf.unsa.ba>
2019-06-18 16:41:43 +0200

    ajdepls

---
+++ TaxiDispecer/Models/Klijent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaxiDispecer.Models
+{
+    public class Klijent : Osoba
+    {
+        public int IdKlijenta { get; set; }
+    }
+}
